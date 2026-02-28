ab547c9ecd467e987f9e1f7896700b9b9fac961c
varshanroshan <varshan.sivanat@gmail.com>
2026-02-28 16:02:47 +0100

    sync from Antigravity

---
+++ project/Cours1.ps1
@@ -0,0 +1,23 @@
+﻿
+$directoryPath = "C:\Users\varsh"
+$filePath = Join-Path -Path $directoryPath -ChildPath "monfichier.txt"
+
+
+if (Test-Path $directoryPath) {
+    Write-Output "Le répertoire existe"
+} else {
+    Write-Output "Le répertoire n'existe pas"
+}
+
+New-Item -Path $filePath -ItemType File
+Write-Output "Le fichier a été créé avec succès"
+
+
+$files = Get-ChildItem -Path $directoryPath -File
+foreach ($file in $files) {
+    Write-Output "Nom du fichier : $($file.Name), Taille : $($file.Length) octets"
+}
+
+if (-not $files) {
+    Write-Output "Le répertoire est vide"
+}
